7893334d4d81627fd0d3308b1555f484764829b7
Hassaan Arain <rafatnaz21@gmail.com>
2024-11-10 12:37:51 +0500

    code pushed

---
+++ src/app/layouts/Header.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import "../layouts/Header.css";
+function Header() {
+  return (
+    <div style={{ background: "#1c1c1e", width: "100%" }}>
+      <main
+        className="flexJustifyAround"
+        style={{
+          textAlign: "center",
+          background: "#1c1c1e",
+          paddingTop: "16px",
+          paddingBottom: "16px",
+          alignItems: "center",
+        }}
+      >
+        <Link href={"/"}>
+          {" "}
+          <h2
+            style={{
+              fontWeight: "800",
+              fontFamily: "monospace",
+              fontSize: "1.5rem",
+              color: "#ff9500",
+            }}
+          >
+            Muhammad Hassaan.
+          </h2>
+        </Link>
+        <nav
+          style={{
+            fontWeight: "700",
+          }}
+        >
+          <Link className="hoverAccentOrange spaceX5 mr" href="/">
+            Home
+          </Link>
+          <Link className="hoverAccentOrange spaceX5 mr" href="/skills">
+            Skills
+          </Link>
+          <Link className="hoverAccentOrange spaceX5 mr" href="/projects">
+            Projects
+          </Link>
+          <Link className="hoverAccentOrange spaceX5 mr" href="/about">
+            About
+          </Link>
+          <Link className="hoverAccentOrange spaceX5 mr" href="/contact">
+            Contact
+          </Link>
+        </nav>
+      </main>
+    </div>
+  );
+}
+
+export default Header;
