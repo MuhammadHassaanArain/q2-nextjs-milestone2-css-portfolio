9d47113ba5b44d0c2ec8fe9c153c20b6f400223b
Hassaan Arain <rafatnaz21@gmail.com>
2024-11-10 21:39:24 +0500

    code pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import HomePage from "@/components/HomePage";
 import Skills from "./skills/page";
 import Contact from "./contact/page";
-// import Projects from "./projects/page";
+import Projects from "./projects/page";
 export default function Home() {
   return (
 <main>
   <HomePage />
   <Skills />
-  {/* <Projects/> */}
+  <Projects/>
   <Contact />
 </main>
   );
